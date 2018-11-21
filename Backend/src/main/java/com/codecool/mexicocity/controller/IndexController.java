c11d9ec802246061e7c9c9ae8519c8bc703d6236
MattHowdy <mate.hodi9@gmail.com>
2018-11-21 15:28:24 +0100

    Server set up and an index page rendered

---
+++ Backend/src/main/java/com/codecool/mexicocity/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.codecool.mexicocity.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = {"/"})
+public class IndexController extends HttpServlet {
+
+        @Override
+        protected void doGet(
+                HttpServletRequest request,
+                HttpServletResponse response)
+                throws ServletException, IOException {
+
+                response.setContentType("application/json");
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().println("{ \"status\": \"ok\"}");
+        }
+}
