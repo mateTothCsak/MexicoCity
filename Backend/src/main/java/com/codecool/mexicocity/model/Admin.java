66e50b6e2d8bd982be301cecd5138ac550589809
stefanrakic001 <stefan.rakic001@gmail.com>
2018-11-22 11:15:37 +0100

    database init completed

--- Backend/src/main/java/com/codecool/mexicocity/model/Admin.java
+++ Backend/src/main/java/com/codecool/mexicocity/model/Admin.java
@@ -26,20 +26,54 @@ public class Admin {
     }
 
 
-    private List<Item> addItemsToFreeShop(EntityManager em) {
+    private List<Item> initDatabase(EntityManager em) {
         EntityTransaction transaction = em.getTransaction();
         transaction.begin();
 
         List<Item> items = new ArrayList<>();
 
-        Item stick = new Item("Pimp Stick", "Nice pimp stick", 100, "image/pimpStick.jpg", Category.FEATHER);
+        //Items:
+        //PIMP
+
+        Item stick = new Item("Pimp Stick", "Nice pimp stick", 1000, "image/pimpStick.jpg", Category.WEAPON);
         em.persist(stick);
+        Item hat = new Item("Pimp Hat", "Beautiful pimp hat", 2500, "image/pimpHat.jpg", Category.HEADGEAR);
+        em.persist(hat);
+        Item fur = new Item("Pimp Fur", "Nice pimp warm coat", 1000, "image/pimpFur.jpg", Category.ARMOR);
+        em.persist(fur);
+        Item sealRing = new Item("Seal-Ring", "", 1000, "image/seal-ring.jpg", Category.RINGS);
+        em.persist(sealRing);
 
-        Rooster rooster = new Rooster();
-        em.persist(rooster);
+        //Soldier
 
-        User user = new User("admin","admin",rooster);
-        em.persist(user);
+        Item helmet = new Item("Soldier helmet", "It is useful sometimes", 100, "image/soldierHelmet.jpg", Category.HEADGEAR);
+        em.persist(helmet);
+        Item kalasnikov = new Item("Soldier Kalasnikov", "Bumm Bumm", 1000, "image/kalasnikov.jpg", Category.WEAPON);
+        em.persist(kalasnikov);
+        Item vest = new Item("Soldier Vest", "Prrrotection 100", 450, "image/vest.jpg", Category.ARMOR);
+        em.persist(vest);
+
+        //Roosters
+
+        Rooster rooster1 = new Rooster();
+        em.persist(rooster1);
+        Rooster rooster2 = new Rooster();
+        em.persist(rooster2);
+        Rooster rooster3 = new Rooster();
+        em.persist(rooster3);
+        Rooster rooster4 = new Rooster();
+        em.persist(rooster4);
+
+        //Users
+
+        User tocsi = new User("tocsi","admin",rooster1);
+        em.persist(tocsi);
+        User mate = new User("mate","admin",rooster2);
+        em.persist(mate);
+        User henry = new User("henry","admin",rooster3);
+        em.persist(henry);
+        User stefan = new User("stefan","admin",rooster4);
+        em.persist(stefan);
 
         transaction.commit();
         items.add(stick);
@@ -48,7 +82,7 @@ public class Admin {
 
     private void createShop(EntityManager em) {
         FreeShop freeShop = new FreeShop();
-        freeShop.addShopItems(addItemsToFreeShop(em));
+        freeShop.addShopItems(initDatabase(em));
     }
 
 
--- Backend/src/main/java/com/codecool/mexicocity/model/Category.java
+++ Backend/src/main/java/com/codecool/mexicocity/model/Category.java
@@ -1,5 +1,5 @@
 package com.codecool.mexicocity.model;
 
 public enum Category {
-    HEADGEAR, ARMOR, BEAK, FEATHER, RINGS, SPURS
+    HEADGEAR, ARMOR, BEAK, WEAPON, RINGS, SPURS
 }
