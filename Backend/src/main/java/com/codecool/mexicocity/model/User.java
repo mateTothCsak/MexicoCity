6593180a3cf63054cdf319c36509bfa082c67281
Mate Toth-Csak <tothcsakmate@gmail.com>
2018-11-22 13:04:01 +0100

    password hashing implemented (with very simple salt mechanism)

--- Backend/src/main/java/com/codecool/mexicocity/model/User.java
+++ Backend/src/main/java/com/codecool/mexicocity/model/User.java
@@ -1,6 +1,9 @@
 package com.codecool.mexicocity.model;
 
 import javax.persistence.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Entity
 @Table(name = "player")
@@ -16,14 +19,34 @@ public class User {
     @OneToOne
     private Rooster myRooster;
 
+    private String salt = "iocnqsmo23n";
+
 
     public User(String email, String password, Rooster rooster) {
         this.email = email;
-        this.password = password;
+        this.password = generateHash(password, salt);
         this.myRooster = rooster;
     }
 
 
-    public User() {
+    public User() {}
+
+
+    public String generateHash(String passwordToHash, String   salt){
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt.getBytes(StandardCharsets.UTF_8));
+            byte[] bytes = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++){
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        return generatedPassword;
     }
 }
