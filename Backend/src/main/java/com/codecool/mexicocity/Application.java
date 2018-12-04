c9d14be28f0f87fce8909be5a7d97acbb7ea0fa0
henrynguyen <henreye@gmail.com>
2018-12-04 13:41:52 +0100

    adding servlets

--- Backend/src/main/java/com/codecool/mexicocity/Application.java
+++ Backend/src/main/java/com/codecool/mexicocity/Application.java
@@ -1,6 +1,9 @@
 package com.codecool.mexicocity;
 
 
+import com.codecool.mexicocity.controller.IndexController;
+import com.codecool.mexicocity.controller.LoggedInMainPageController;
+import com.codecool.mexicocity.controller.MyProfileController;
 import com.codecool.mexicocity.controller.RegistrationController;
 import com.codecool.mexicocity.service.RoosterService;
 import com.codecool.mexicocity.service.UserService;
@@ -24,8 +27,10 @@ public class Application implements ServletContextListener {
         UserService userService = new UserService();
 
         ServletContext context = sce.getServletContext();
-        context.addServlet("RegistrationController", new RegistrationController(em, roosterService, userService)).addMapping("/register");
-    }
+        context.addServlet("Index", new IndexController()).addMapping("/");
+        context.addServlet("LoggedInHome", new LoggedInMainPageController()).addMapping("/home");
+        context.addServlet("MyProfile", new MyProfileController()).addMapping("/myprofile");
+        context.addServlet("Registration", new RegistrationController(em, roosterService, userService)).addMapping("/register");}
 
 
     @Override
