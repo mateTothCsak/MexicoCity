397c18739de1d37d6ccd72c55f07646d1d57b461
MattHowdy <mate.hodi9@gmail.com>
2018-12-06 13:36:15 +0100

    layout refactored + alt tags added

--- Frontend/mexico-city/src/components/Layout/Layout.js
+++ Frontend/mexico-city/src/components/Layout/Layout.js
@@ -3,13 +3,15 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Footer from '../Footer/Footer';
 
 const layout = (props) => (
     <Aux>
-        <Toolbar/>
+        <Toolbar displayModal={props.displayModal}/>
         <main className={classes.Content}>
             {props.children}
         </main>
+        <Footer/>
     </Aux>
 );
 
