86b718aa0a5ccb964289cce2eda791d8b19a59c5
Mate Toth-Csak <tothcsakmate@gmail.com>
2018-12-06 10:40:23 +0100

    LogIn with validation implemented Salt is now randomized and unique for each user Globals class with SecureRandom object created

--- Backend/src/main/java/com/codecool/mexicocity/controller/IndexController.java
+++ Backend/src/main/java/com/codecool/mexicocity/controller/IndexController.java
@@ -1,14 +1,10 @@
 package com.codecool.mexicocity.controller;
 
-import com.codecool.mexicocity.dao.UserDao;
-import com.codecool.mexicocity.model.User;
 import com.codecool.mexicocity.service.UserService;
 import com.codecool.mexicocity.util.JsonHandler;
-import com.codecool.mexicocity.util.MyEntityManagerFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,11 +13,6 @@ import java.io.IOException;
 
 public class IndexController extends HttpServlet {
     UserService userService;
-
-    // For some reason this will be implemented, no matter if we pass in arguments
-    public IndexController(){
-        this.userService = new UserService(new UserDao(MyEntityManagerFactory.getInstance()));
-    }
 
     public IndexController(UserService userService) {
         this.userService = userService;
@@ -47,8 +38,7 @@ public class IndexController extends HttpServlet {
         String enteredEmail = node.get("email").textValue();
         String enteredPassword = node.get("password").textValue();
 
-        User user = userService.getUserByEmail(enteredEmail);
-        String realPassword = user.getPassword();
+        userService.tryLogIn(enteredEmail, enteredPassword, response);
 
     }
 }
--- Backend/src/main/java/com/codecool/mexicocity/service/UserService.java
+++ Backend/src/main/java/com/codecool/mexicocity/service/UserService.java
@@ -3,9 +3,14 @@ package com.codecool.mexicocity.service;
 import com.codecool.mexicocity.dao.UserDao;
 import com.codecool.mexicocity.model.Rooster;
 import com.codecool.mexicocity.model.User;
+import com.codecool.mexicocity.util.Globals;
+import com.codecool.mexicocity.util.JsonHandler;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 public class UserService {
@@ -47,5 +52,28 @@ public class UserService {
     public void createUser(String email, String password, Rooster rooster) {
         User user = new User(email, password, rooster);
         add(user);
+    }
+
+    public void tryLogIn(String enteredEmail, String enteredPassword, HttpServletResponse response) throws IOException {
+        if(getUserByEmail(enteredEmail) != null) {
+            User user = getUserByEmail(enteredEmail);
+            String realPassword = user.getPassword();
+            String salt = user.getSalt();
+
+            if (realPassword.equals(user.generateHash(enteredPassword, salt))) {
+                String userJsonString = JsonHandler.getInstance().jsonify(user);
+                response.setContentType("application/json;charset=UTF-8");
+                ServletOutputStream out = response.getOutputStream();
+                out.print(userJsonString);
+            } else {
+                System.out.println("Email does not exist or wrong password");
+                ServletOutputStream out = response.getOutputStream();
+                out.print("");
+            }
+        } else {
+            System.out.println("Email does not exist or wrong password");
+            ServletOutputStream out = response.getOutputStream();
+            out.print("");
+        }
     }
 }
