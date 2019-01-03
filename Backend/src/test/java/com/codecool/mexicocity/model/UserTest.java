da5cc7b963579a751d77c4044fe6aae62745dc5a
henrynguyen <henreye@gmail.com>
2019-01-03 11:15:06 +0100

    Test User get email

---
+++ Backend/src/test/java/com/codecool/mexicocity/model/UserTest.java
@@ -0,0 +1,41 @@
+package com.codecool.mexicocity.model;
+
+import com.codecool.mexicocity.dao.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UserTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void whenFindByName_ThenReturnUserName(){
+
+        //given
+        Rooster rooster = new Rooster();
+        entityManager.persist(rooster);
+        User alex = new User("alex@gmail.com", "123", rooster);
+        entityManager.persist(alex);
+        entityManager.flush();
+
+        //when
+        User foundAlex = userRepository.findUserByEmail("alex@gmail.com");
+
+        //then
+        assertEquals(alex.getEmail(), foundAlex.getEmail());
+
+    }
+}
